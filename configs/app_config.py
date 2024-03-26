9771ef81617d65e89690928214f851e2a7855373
chiemezie1 <chiemezieagbo1@gmail.com>
2024-03-26 05:33:52 +0100

    configs/

---
+++ configs/app_config.py
@@ -0,0 +1,13 @@
+# config.py
+
+# Path to the directory containing transaction JSON files
+PATH_TO_MEMPOOL = "mempool/"
+
+# The specified mining difficulty target
+DIFFICULTY_TARGET = "0000ffff00000000000000000000000000000000000000000000000000000000"
+
+# The standard block reward value (example value in Satoshis, adjust as necessary)
+BLOCK_REWARD = 6.25 * 10**8  # 6.25 BTC, converted to Satoshis
+
+# Configuration for block size limit
+BLOCK_SIZE_LIMIT = 1_000_000  # 1 MB limit
